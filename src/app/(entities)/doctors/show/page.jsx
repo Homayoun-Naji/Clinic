11ca4fd9d2bcfa93cfef2ffdc60470098396a5a1
Homayoun Naji <hjnrokhi@gmail.com>
2026-06-10 22:26:47 +0330

    fix: apply patients changes for other entities

--- src/app/(entities)/doctors/show/page.jsx
+++ src/app/(entities)/doctors/show/page.jsx
@@ -1,30 +1,49 @@
 import ShowCard from "@/app/components/ShowCard";
+import axios from "axios";
 
 export const metadata = {
   title: "Show Doctors",
 };
 
-const doctorsData = [
-  [
-    { title: "First Name", value: "Homayoun" },
-    { title: "Last Name", value: "Naji" },
-    { title: "Specialization", value: "Doctor" },
-    { title: "Phone", value: "09010644801" },
-    { title: "Email", value: "hjnrokhi@gmail.com" },
-  ],
-  [
-    { title: "First Name", value: "Marjan" },
-    { title: "Last Name", value: "Dehghan" },
-    { title: "Specialization", value: "Professor" },
-    { title: "Phone", value: "09151097687" },
-    { title: "Email", value: "bidad.zaman12@gmail.com" },
-  ],
+const doctorsData = await axios
+.get("http://localhost:3000/api/doctors")
+.then((res) => res.data)
+.catch((err) => {
+  console.error(err);
+  return [];
+});
+
+const doctorsTitle = [
+  "First Name",
+  "Last Name",
+  "Specialization",
+  "Phone",
+  "Email",
 ];
+
+const doctorsKeys = [
+  "first_name",
+  "last_name",
+  "specialization",
+  "phone",
+  "email",
+];
+
+function doctorsDataGenerator(data) {
+  return data.map((doctor) =>
+    doctorsKeys.map((key, index) => ({
+      title: doctorsTitle[index],
+      value: doctor[key],
+    }))
+  );
+}
+
+const generatedDoctorsData = doctorsDataGenerator(doctorsData);
 
 export default function DoctorsShow() {
   return (
     <div className="grid grid-cols-4 gap-8 p-8 mt-12">
-      {doctorsData.map((doctor, index) => {
+      {generatedDoctorsData.map((doctor, index) => {
         return <ShowCard key={index} data={doctor} />;
       })}
     </div>
--- src/app/(entities)/medicines/show/page.jsx
+++ src/app/(entities)/medicines/show/page.jsx
@@ -1,28 +1,48 @@
 import ShowCard from "@/app/components/ShowCard";
+import axios from "axios";
 
 export const metadata = {
   title: "Show Medications",
 };
 
-const medicinesData = [
-  [
-    { title: "Name", value: "Cold Stop" },
-    { title: "Description", value: "For disease" },
-    { title: "Price", value: 150000 },
-    { title: "Stock", value: 50 },
-  ],
-  [
-    { title: "Name", value: "C Vitamin" },
-    { title: "Description", value: "Vitamin" },
-    { title: "Price", value: 50000 },
-    { title: "Stock", value: 50 },
-  ],
+const medicinesData = await axios
+.get("http://localhost:3000/api/medicines")
+.then((res) => res.data)
+.catch((err) => {
+  console.error(err);
+  return [];
+});
+
+const medicinesTitle = [
+  "Name",
+  "Description",
+  "Price",
+  "Stock",
 ];
+
+const medicinesKeys = [
+  "name",
+  "description",
+  "price",
+  "stock",
+];
+
+function medicinesDataGenerator(data) {
+  return data.map((medicine) =>
+    medicinesKeys.map((key, index) => ({
+      title: medicinesTitle[index],
+      value: medicine[key],
+    }))
+  );
+}
+
+const generatedMedicinesData = medicinesDataGenerator(medicinesData);
+
 
 export default function MedicinesShow() {
   return (
     <div className="grid grid-cols-4 gap-8 p-8 mt-12">
-      {medicinesData.map((medicine, index) => {
+      {generatedMedicinesData.map((medicine, index) => {
         return <ShowCard key={index} data={medicine} />;
       })}
     </div>
