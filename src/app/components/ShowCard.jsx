742a622d832fca862d28cf0e3085c3e521258683
Homayoun Naji <hjnrokhi@gmail.com>
2026-06-10 20:58:16 +0330

    fix: fix db connection

--- src/app/components/ShowCard.jsx
+++ src/app/components/ShowCard.jsx
@@ -1,6 +1,8 @@
 export default function ShowCard({ data }) {
+  console.log(data)
   return (
     <div className="flex flex-col bg-secondary gap-2 p-4 rounded-2xl">
+      
       {data.map((item, index) => {
         return (
           <p key={index}>
--- src/app/patients/show/page.jsx
+++ src/app/patients/show/page.jsx
@@ -1,37 +1,44 @@
 import ShowCard from "@/app/components/ShowCard";
+import axios from "axios";
 
 export const metadata = {
   title: "Show Patients",
-};
 
-// async function getData() {
-//   const res = await fetch("http://localhost:3000/api/patients")  
+  // async function getData() {
+};
+//   const res = await axios("http://localhost:3000/api/patients")
 //   console.log(res)
 // }
 
 // const patientsData = true;
 
-const patientsData = [
-  [
-    { title: "First Name", value: "Ali" },
-    { title: "Last Name", value: "Mohammadi" },
-    { title: "Birth Date", value: "11/03/2008" },
-    { title: "Disease", value: "Illness" },
-  ],
-  [
-    { title: "First Name", value: "Ahmad" },
-    { title: "Last Name", value: "Rezaei" },
-    { title: "Birth Date", value: "05/29/2000" },
-    { title: "Disease", value: "Illness" },
-  ],
-];
+// const patientsData = [
+//   [
+//     { title: "First Name", value: "Ali" },
+//     { title: "Last Name", value: "Mohammadi" },
+//     { title: "Birth Date", value: "11/03/2008" },
+//     { title: "Disease", value: "Illness" },
+//   ],
+//   [
+//     { title: "First Name", value: "Ahmad" },
+//     { title: "Last Name", value: "Rezaei" },
+//     { title: "Birth Date", value: "05/29/2000" },
+//     { title: "Disease", value: "Illness" },
+//   ],
+// ];
 
-export default function PatientsShow() {
+export default async function PatientsShow() {
+  const patientsData = await axios
+    .get("http://localhost:3000/api/patients")
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+
   return (
     <div className="grid grid-cols-4 gap-8 p-8 mt-12">
-      {patientsData.map((patient, index) => {
-        return <ShowCard key={index} data={patient} />;
-      })}
+      {/* {patientsData.map((patient, index) => { */}
+        {/* return <ShowCard key={index} data={patient} />; */}
+        <ShowCard data={patientsData} />
+      {/* })} */}
     </div>
   );
 }
